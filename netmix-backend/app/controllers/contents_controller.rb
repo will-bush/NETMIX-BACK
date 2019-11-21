2acf3663b57ac56d0999b3c1cea0559ddd676473
Will Bush <williamdcbush@gmail.com>
2019-11-21 12:02:35 +0000

    actions added to content and listings controllers

--- netmix-backend/app/controllers/contents_controller.rb
+++ netmix-backend/app/controllers/contents_controller.rb
@@ -1,2 +1,18 @@
 class ContentsController < ApplicationController
+
+    def index
+        contents = Content.all
+        render json: contents
+    end
+
+    def create
+        content = Content.create(content_params)
+        render json: content
+    end
+
+    private
+
+    def content_params
+        params.require(:content).permit(:Title, :Year, :Rated, :Released, :Runtime, :Genre, :Director, :Writer, :Actors, :Plot, :Language, :Country, :Awards, :Poster, :Ratings, :Metascore, :imdbRating, :imdbVotes, :imdbID, :Type, :DVD, :BoxOffice, :Production, :Website, :Response, :totalSeasons)
+    end
 end
--- netmix-backend/app/controllers/listings_controller.rb
+++ netmix-backend/app/controllers/listings_controller.rb
@@ -1,2 +1,19 @@
 class ListingsController < ApplicationController
+
+    def index
+        listings = Listing.all
+        render json: listings
+    end
+
+    def create
+        listing = Listing.create(listing_params)
+        render json: listing
+    end
+
+    private
+
+    def listing_params
+        params.require(:listing).permit(:list_id, :content_id, :review, :review_score)
+    end
+
 end
